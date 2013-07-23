63a19c7fe326f7472e20c99973d6c207755fe56c
Dan Melton <melton.dan@gmail.com>
2013-07-23 14:42:35 -0700

    adding procfile

--- recipes/server.rb
+++ recipes/server.rb
@@ -7,3 +7,33 @@ remote_file File.join(node['selenium']['server']['installpath'], 'selenium-serve
   action :create
   mode 0644
 end
+
+directory "/var/run/#{new_resource.name}" do
+  owner 'root'
+  group 'root'
+  mode '0755'
+  recursive true
+  action :create
+end
+
+directory "/var/log/#{new_resource.name}" do
+  owner 'root'
+  group 'root'
+  mode '0755'
+  recursive true
+  action :create
+end
+
+template 'procfile.init' do
+  path "/etc/init.d/#{new_resource.name}-#{type}"
+  owner 'root'
+  group 'root'
+  mode '0755'
+  variables ({
+    :name => new_resource.name,
+    :type => type,
+    :current_path => ::File.join(new_resource.application.path, 'current'),
+    :command => pf[type.to_s]
+  })
+end
+
